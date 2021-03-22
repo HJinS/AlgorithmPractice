a297e5306fcce381875a73bca0ad8854f5d05b9e
unknown <windowed@sju.ac.kr>
2021-03-22 15:35:33 +0900

    210322

---
+++ AlgorithmMiddle/divide_conquer/1074-re.py
@@ -0,0 +1,26 @@
+N, R, C = map(int, input().split())
+
+count = 0
+
+while N > 0:
+    tmp = (2 ** N)//2
+    if N > 1:
+        if tmp > R and tmp <= C:
+            count += tmp ** 2
+            C -= tmp
+        elif tmp <= R and tmp > C:
+            count += (tmp ** 2) * 2
+            R -= tmp
+        elif tmp <= R and tmp <= C:
+            count += (tmp**2) * 3
+            R -= tmp
+            C -= tmp
+    elif N == 1:
+        if R == 0 and C == 1:
+            count += 1
+        elif R == 1 and C == 0:
+            count += 2
+        elif R == 1 and C == 1:
+            count += 3
+    N -= 1
+print(count)
