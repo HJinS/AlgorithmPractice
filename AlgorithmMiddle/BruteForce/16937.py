dc852854e3bf9c0dbb77bb11c971cbcbb997c5ab
unknown <windowed@sju.ac.kr>
2021-04-28 13:39:06 +0900

    210428

---
+++ AlgorithmMiddle/BruteForce/16937.py
@@ -0,0 +1,32 @@
+H, W = map(int, input().split())
+N = int(input())
+stickers = []
+for i in range(N):
+    stickers.append(list(map(int, input().split())))
+
+#최대값이니까 나열했을 경우 범위가 벗어나지 않는지만 확인하면 된다.
+def check(s1, s2):
+    if s1[0] + s2[0] <= W and max(s1[1], s2[1]) <= H:
+        return True
+    if s1[1] + s2[1] <= H and max(s1[0], s2[0]) <= W:
+        return True
+    return False
+
+ans = 0
+
+for i in range(N-1):
+    for j in range(i+1, N):
+        s1 = stickers[i]
+        s2 = stickers[j]
+        area = s1[0] * s1[1]  + s2[0] * s2[1]
+        for k in range(1 << 2):
+            s1 = stickers[i]
+            s2 = stickers[j]
+            if 1 << 1 & k:
+                s1 = list(reversed(stickers[i]))
+            if 1 << 0 & k: 
+                s2 = list(reversed(stickers[j]))
+            if check(s1, s2):
+                ans = max(area, ans)
+
+print(ans)
